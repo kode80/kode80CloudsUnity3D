74947bd780211ef45a40770d1d507a86e17c7c1b
Ben Hopkins <ben@kode80.com>
2016-04-14 10:33:23 -0400

    Removed Unity Inputs from demo's FlyCamera script, replaced with hardcoded input handling (fixes flooding errors/not being able to try demo without setting up correct inputs)

--- Assets/kode80/Clouds/Examples/Demo/Scripts/FlyCamera.cs
+++ Assets/kode80/Clouds/Examples/Demo/Scripts/FlyCamera.cs
@@ -37,6 +37,15 @@ namespace kode80.Clouds.Examples
 		private float _rotationY = 0.0f;
 		private Vector3 _targetPosition;
 
+		// Can't rely on Unity Inputs being defined,
+		// so use hardcoded input handling to avoid
+		// flooding errors and allow user to try demo
+		// without any need for setup.
+		private Vector3 _movement;
+		private bool _isRunning;
+		private Vector2 _mousePrev;
+		private Vector2 _mouseDelta;
+
 		void Start () 
 		{
 			_targetPosition = transform.position;
@@ -46,19 +55,42 @@ namespace kode80.Clouds.Examples
 		
 		void Update ()
 		{
-			_rotationX += Input.GetAxis("Mouse X") * Time.deltaTime * lookSpeed;
-			_rotationY += Input.GetAxis("Mouse Y") * Time.deltaTime * lookSpeed;
+			UpdateInput();
+
+			_rotationX += _mouseDelta.x * Time.deltaTime * lookSpeed;
+			_rotationY += _mouseDelta.y * Time.deltaTime * lookSpeed;
 			_rotationY = Mathf.Clamp( _rotationY, -90, 90);
 			
 			transform.localRotation = Quaternion.AngleAxis( _rotationX, Vector3.up);
 			transform.localRotation *= Quaternion.AngleAxis( _rotationY, Vector3.left);
 
-			float run = Input.GetButton( "Run") ? runMultiplier : 1.0f;
-			_targetPosition += transform.forward * moveSpeed * run * Time.deltaTime * Input.GetAxis("Forward");
-			_targetPosition += transform.right * moveSpeed * run * Time.deltaTime * Input.GetAxis("Horizontal");
-			_targetPosition += transform.up * moveSpeed * run * Time.deltaTime * Input.GetAxis("Vertical");
+			float run = _isRunning ? runMultiplier : 1.0f;
+			_targetPosition += transform.forward * moveSpeed * run * Time.deltaTime * _movement.z;
+			_targetPosition += transform.right * moveSpeed * run * Time.deltaTime * _movement.x;
+			_targetPosition += transform.up * moveSpeed * run * Time.deltaTime * _movement.y;
 	
 			transform.position = Vector3.Lerp( transform.position, _targetPosition, 0.5f); 
+		}
+
+		void UpdateInput()
+		{
+			if( Input.GetKey( KeyCode.W)) { _movement.z = 1.0f; }
+			else if( Input.GetKey( KeyCode.S)) { _movement.z = -1.0f; }
+			else { _movement.z = 0.0f; }
+
+			if( Input.GetKey( KeyCode.D)) { _movement.x = 1.0f; }
+			else if( Input.GetKey( KeyCode.A)) { _movement.x = -1.0f; }
+			else { _movement.x = 0.0f; }
+
+			if( Input.GetKey( KeyCode.E)) { _movement.y = 1.0f; }
+			else if( Input.GetKey( KeyCode.Q)) { _movement.y = -1.0f; }
+			else { _movement.y = 0.0f; }
+
+			_isRunning = Input.GetKey( KeyCode.LeftShift);
+
+			Vector2 mouse = (Vector2)Input.mousePosition;
+			_mouseDelta = mouse - _mousePrev;
+			_mousePrev = mouse;
 		}
 	}
 }
